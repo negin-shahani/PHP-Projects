07bc6d4be29f4a0b2832df61f10c1a4b8ccf17f6
negin-titanium <neginshahany@gmail.com>
2022-07-31 21:25:23 +0430

    if input is correct send me the email with message

--- Contact Form using HTML, CSS, JS,PHP/message.php
+++ Contact Form using HTML, CSS, JS,PHP/message.php
@@ -1,3 +1,29 @@
 <?php
-echo "This message is sent from PHP file!";
+//first get the values from form
+$name = $_POST['Name'];
+$email = $_POST['Email'];
+$phone = $_POST['Phone'];
+$website = $_POST['Website'];
+$message = $_POST['message'];
+
+//if email and message is not empty and email is valid
+if(!empty($email) && !empty($message)){
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $reciever = "neginshahany@gmail.com"; //reciever of the info is my email
+        $subject = "From:  $name <$email>"; //the subject of the email
+        $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\nMessage: $message";
+        $sender = "From: $email"; //the email of the sender
+        //mail is an in-build function to send eamil
+        if(mail($reciever, $subject, $body, $sender)){
+            echo "Your message has been sent!";
+        }else{
+            echo "Sorry, failed to send your message!";
+        }
+    }else{
+        echo "Enter a valid Email address!";
+    }
+}else{
+    echo "Email and password feild is required!";
+}
+
 ?>
--- Contact Form using HTML, CSS, JS,PHP/script.js
+++ Contact Form using HTML, CSS, JS,PHP/script.js
@@ -4,6 +4,7 @@ statusText = form.querySelector(".buttons span");
 form.onsubmit = (e) =>{
     e.preventDefault(); //prevent the form from submiting
     statusText.style.display = "block";
+    statusText.style.color = "#957DAD";
 
     // we want to use ajax to send form data of html to php
     let xml = new XMLHttpRequest();
@@ -11,7 +12,19 @@ form.onsubmit = (e) =>{
     xml.onload = () =>{ //once ajax loaded
        if(xml.readyState == 4 && xml.status == 200){ //it means if there is no error
             let response = xml.response;
-            console.log(response); 
+
+            if(response.indexOf("Sorry, failed to send your message!") !=-1 
+            || response.indexOf("Enter a valid Email address!") !=-1 
+            || response.indexOf("Email and password feild is required!") !=-1){
+                statusText.style.color = "red";
+            }else{
+                form.reset(); //if the message is sent reset the input of form
+                setTimeout(() => { //after 3 seconds stop showing the sent message
+                    statusText.style.display = "none";
+                }, 3000);
+            }
+
+            statusText.innerHTML = response;
        } 
     }
     //create new formData obj. This obj is used to send form data.
