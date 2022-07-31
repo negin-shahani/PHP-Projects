5221faf91ed1910081a3ad0d711eae010a56bbd4
negin-titanium <neginshahany@gmail.com>
2022-07-31 20:18:18 +0430

    add changes to script.js and add message.php

---
+++ Contact Form using HTML, CSS, JS,PHP/script.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("form"),
+statusText = form.querySelector(".buttons span");
+
+form.onsubmit = (e) =>{
+    e.preventDefault(); //prevent the form from submiting
+    statusText.style.display = "block";
+
+    // we want to use ajax to send form data of html to php
+    let xml = new XMLHttpRequest();
+    xml.open("POST", "message.php", true); //sending post request to message.php
+    xml.onload = () =>{ //once ajax loaded
+       if(xml.readyState == 4 && xml.status == 200){ //it means if there is no error
+            let response = xml.response;
+            console.log(response); 
+       } 
+    }
+    xml.send();
+}
