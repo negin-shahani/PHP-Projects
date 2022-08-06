d113ea108799ad399fb53e3a30b18fd8dba7d872
negin-titanium <neginshahany@gmail.com>
2022-08-06 22:39:55 +0430

    login form init

--- Login & Signup Form/Login-user.php
+++ Login & Signup Form/Login-user.php
@@ -1,5 +1,8 @@
 <?php
+//have to controll the user data!
 ?>
+
+//have to build these pages: controllerUserData.php, forgot-password.php, signup-user.php
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,6 +14,27 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form login-form">
+                <form action="login-user.php" method="post" autocomplete="">
+                    <h2 class="text-center">Login Form</h2>
+                    <p class="text-center">Login with your email and password.</p>
+                    //here should show errors
+                    <div class="form-group">
+                        <input type="email" name="email" class="form-control" required placeholder="Email Address">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" name="password" class="form-control" required placeholder="Password" >
+                    </div>
+                    <div class="link forget-pass text-left"><a href="forgot-password.php">Forgot password?</a></div>
+                    <div class="form-group">
+                        <input type="submit" name="login" class="form-control" value="login" >
+                    </div>
+                    <div class="link login-link text-center">Not yet a member? <a href="signup-user.php">Signup now</a></div>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
