5e5b1e5f5a07a01a521568e30c603a4d95c16464
negin-titanium <neginshahany@gmail.com>
2022-08-06 21:48:39 +0430

    add bootstarp to project

--- Login & Signup Form/Login-user.php
+++ Login & Signup Form/Login-user.php
@@ -5,10 +5,10 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Login page</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <body>
     
